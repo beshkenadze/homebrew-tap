5906c8d213912550d028cffecb2e69efc3440682
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2026-01-25 23:55:52 +0200

    kaban 0.2.11
    Automated update from https://github.com/beshkenadze/kaban

--- Formula/kaban.rb
+++ Formula/kaban.rb
@@ -1,8 +1,8 @@
 class Kaban < Formula
   desc "Terminal Kanban for AI Code Agents"
   homepage "https://beshkenadze.github.io/kaban"
-  url "https://registry.npmjs.org/@kaban-board/cli/-/cli-0.2.9.tgz"
-  sha256 "02c49a99a3ea2f859d8e4a5c9ce2fe1ae817deb5a8d0ce779b96b8b32622b3bd"
+  url "https://registry.npmjs.org/@kaban-board/cli/-/cli-0.2.11.tgz"
+  sha256 "3b2b6827b58afaf234ab7c0601272e3aa4f606c0a138b6f26d857cb1af447d17"
   license "MIT"
 
   depends_on "node"
